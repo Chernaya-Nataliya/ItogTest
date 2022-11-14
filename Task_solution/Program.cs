0834f8939ce3dec3a572127714a625708df3ec2c
Nataliya <chernishmail@gmail.com>
2022-11-14 11:10:14 +0300

    Создали папку Task_solution и написали программу на С# для решения задачи

---
+++ Task_solution/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using static System.Console;
+
+string[] array = AskArray();
+string[] result = FindLessThan(array, 3);
+WriteLine($"[{string.Join(", ", array)}] -> [{string.Join(", ", result)}]");
+
+string[] FindLessThan(string[] input, int a) 
+{
+    string[] output = new string[CountLessThan(input, a)];
+
+    for(int i = 0, j = 0; i < input.Length; i++) 
+    {
+        if(input[i].Length <= a) 
+        {
+            output[j] = input[i];
+            j++;
+        }
+    }
+
+    return output;
+}
+
+int CountLessThan(string[] input, int a) 
+{
+    int count = 0;
+
+    for(int i = 0; i < input.Length; i++) 
+    {
+        if(input[i].Length <= a) 
+        {
+            count++;
+        }
+    }
+
+    return count;
+}
+
+string[] AskArray() 
+{
+    Write("Введите значения через пробел: ");
+    return ReadLine().Split(" ");
+}
